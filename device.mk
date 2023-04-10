bc4a2c4ef65eaa875a21c4ccd4530a7490c929a9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-04-10 15:23:09 +0530

    dubai: Build AOSP NfcNci app
    We removed it from build/make in
    https://github.com/ArrowOS/android_build/commit/24c48e6085b1f8713bc68478ea76167b6dd93e65
    
    This partially reverts commit 757a836dc512c956e4f554d0e361027e28a6468f.

--- device.mk
+++ device.mk
@@ -84,6 +84,7 @@ PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
     libchrome.vendor \
+    NfcNci \
     Tag \
     nfc_nci.st21nfc.default
 
